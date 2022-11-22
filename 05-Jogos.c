b1c78a424af42399eba3d32b683db9d6b5524e43
Arthur Faria <69607826+ArthurFariaz@users.noreply.github.com>
2022-11-22 08:20:12 -0300

    Tarefa  1.4.1

--- 05-Jogos.c
+++ 05-Jogos.c
@@ -18,70 +18,80 @@ int main (int argc, char* args[])
     SDL_bool event = SDL_TRUE;
     int qtd = 0;
     int x,y;
-    int corR[10], corB[10], corG[10];
-    SDL_Rect retangulos[10];
-
+    struct retangulo {
+        SDL_Rect r;
+        int corR;
+        int corG;
+        int corB;
+    };
+    struct retangulo retangulos[10];
     
     while (event) {
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
         SDL_RenderFillRect(ren, &r);
+        
         for(int i = 0; i < qtd;i++){
-            SDL_SetRenderDrawColor(ren, corR[i],corG[i],corB[i],0x00);
-            SDL_RenderFillRect(ren, &retangulos[i]);
+            SDL_SetRenderDrawColor(ren, retangulos[i].corR,retangulos[i].corG,retangulos[i].corB,0x00);
+            SDL_RenderFillRect(ren, &retangulos[i].r);
         }
+
         SDL_RenderPresent(ren);
+        
         SDL_WaitEvent(&evt);
-        if (evt.type == SDL_KEYDOWN) {
-            switch (evt.key.keysym.sym) {
-                case SDLK_UP:
-                    if(r.y == 0){
+        switch (evt.type) {
+            case SDL_KEYDOWN:
+                switch (evt.key.keysym.sym) {
+                    case SDLK_UP:
+                        if(r.y == 0){
+                            break;
+                        }
+                        r.y -= 5;
                         break;
-                    }
-                    r.y -= 5;
-                    break;
-                case SDLK_DOWN:
-                    if(r.y == 490){
+                    case SDLK_DOWN:
+                        if(r.y == 490){
+                            break;
+                        }
+                        r.y += 5;
                         break;
-                    }
-                    r.y += 5;
-                    break;
-                case SDLK_LEFT:
-                    if(r.x == 0){
+                    case SDLK_LEFT:
+                        if(r.x == 0){
+                            break;
+                        }
+                        r.x -= 5;
                         break;
-                    }
-                    r.x -= 5;
-                    break;
-                case SDLK_RIGHT:
-                    if(r.x == 490){
+                    case SDLK_RIGHT:
+                        if(r.x == 490){
+                            break;
+                        }
+                        r.x += 5;
                         break;
-                    }
-                    r.x += 5;
-                    break;
-            }
-        }
-        if (evt.type == SDL_WINDOWEVENT) {
-            switch(evt.window.event){
-            case SDL_WINDOWEVENT_CLOSE:
-            event = SDL_FALSE;
+                }
             break;
-            }
-        }
-        if (evt.type == SDL_MOUSEBUTTONDOWN) {
+            case SDL_WINDOWEVENT:
+                switch(evt.window.event){
+                case SDL_WINDOWEVENT_CLOSE:
+                event = SDL_FALSE;
+                break;
+                }
+            break;
+            case SDL_MOUSEBUTTONDOWN:
             switch(evt.button.button){
                 case SDL_BUTTON_LEFT:
                     if(qtd < 10){
                         SDL_GetMouseState(&x,&y);
-                        SDL_Rect retangulo = {x, y , 10 + rand() % 100,10 + rand() % 100 };
-                        corR[qtd] = rand() % 255;
-                        corG[qtd] = rand() % 255;
-                        corB[qtd] = rand() % 255;
-                        retangulos[qtd] = retangulo;
-                        qtd = qtd + 1;
-
+                        struct retangulo ret;
+                        SDL_Rect r = {x, y , 10 + rand() % 100,10 + rand() % 100 };
+                        ret.r = r;
+                        ret.corR = rand() % 255;
+                        ret.corG = rand() % 255;
+                        ret.corB = rand() % 255;
+                        retangulos[qtd] = ret;
+                        qtd++;                    
                     }
             }
+            break;
         }
     }
 
