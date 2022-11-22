46f39e3ca3e4cdc16f8a20f155746ca0b0a0e400
Arthur Faria <69607826+ArthurFariaz@users.noreply.github.com>
2022-11-22 08:23:12 -0300

    Tarefa 1.5.2

--- 07-Jogos.c
+++ 07-Jogos.c
@@ -1,14 +1,17 @@
-int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms, int delay){
-    int aux = delay;
-    int event = SDL_WaitEventTimeout(evt, delay);
-    if (event){
-    	delay -= (SDL_GetTicks() - *ms);
-    	return event;
-    }else{
-    	delay = aux;
-    	return event;
-    }
+int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* espera, Uint32* ms) {
+	int event = SDL_WaitEventTimeout(evt, *espera);
+	if (event){
+		*espera -= (SDL_GetTicks() - *ms);
+		if(*espera <= 0) {
+			*espera = 0;
+		}
+		return event;
+	}else{
+		return event;
+	}
 }
-// int delay = 500;
-// Uint32 ms = SDL_GetTicks();
-// int isevt = AUX_WaitEventTimeoutCount(&evt, &ms,delay);
+/*
+int espera = 500;
+Uint32 ms = SDL_GetTicks();
+int isevt = AUX_WaitEventTimeoutCount(&evt, &espera , &ms);
+*/
